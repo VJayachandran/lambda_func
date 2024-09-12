c1db92abb895a9c2ca7cd6be139b35ab019cf942
Jayachandran V <165295359+VJayachandran@users.noreply.github.com>
2024-09-12 23:46:01 +0530

    Add files via upload

---
+++ lambda_fun.py
@@ -0,0 +1,12 @@
+#lambda list
+#initializing list
+test_list = ["434", "823", "98", "74"]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# checking all elements to be numeric using isdigit()
+res = all(ele.isdigit() for ele in test_list)
+
+# printing result
+print("Are all strings digits? : " + str(res))
